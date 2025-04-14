dcd781643d4b92790e993e7873aa058b8fc76c46
ZIshmael <38586415+Zaederx@users.noreply.github.com>
2025-04-14 13:10:57 +0200

    Update cookie.ts

--- ts/cookie.ts
+++ ts/cookie.ts
@@ -11,99 +11,209 @@
  * 
  * 
  */
- export class Cookie {
-    name:string
-    value:string|null
-    domain:string
-    path:string
-    expires:string|Date|null
-    // size:string -  size of the cookie - can be left blank 
-    secure:boolean
-    httpOnly:boolean
-    sameSite:string
+export class Cookie {
+   name:string
+   value:string|null
+   domain:string
+   path:string
+   expires:string|Date|null
+   // size:string -  size of the cookie - can be left blank 
+   secure:boolean
+   httpOnly:boolean
+   sameSite:string
 
-    /**
-     * 
-     * @param name name of the cookie
-     * @param value value of the cookie - where you store user identifation like a username (can also be stored as json string of useful information.)
-     * @param domain which hosts the cookie should be sent to while browsing
-     * @param path which path the must be included in the URL at the chosen domain for the cookie be sent to the host - '/' by default
-     * @param expires when the cookie should expire / be deleted from browser - 2 days time by default if not specified
-     * @param secure whether to use https or not (not being http instead)
-     * @param httpOnly whether the cookie should be inaccessible to javascript
-     * @param sameSite whether the cookies can be sent with cross site requests
-     */
-    constructor(name:string, value:string|null, domain:string, path?:string, expires?:string|Date|null, secure?:boolean, httpOnly?:boolean, sameSite?:'Strict'|'Lax'|'None'|null) {
-        this.name = name
-        this.value = value
-        this.domain = domain 
-        this.path = path ? path : '/'
-        //set expiry date
-        const d = new Date();
-        const year = d.getFullYear()
-        const month = d.getMonth()
-        const day = d.getDate()
-        const twoDaysTime = day + 3
-        this.expires = expires != null ? expires : new Date(year,month,twoDaysTime)
-        // this.size = size ? size : 
-        this.secure = secure != null ? secure : false
-        this.httpOnly = httpOnly ? httpOnly : false
-        this.sameSite = sameSite != null ? sameSite : 'Lax'
-    }
-
-
+   /**
+    * 
+    * @param name name of the cookie
+    * @param value value of the cookie - where you store user identifation like a username (can also be stored as json string of useful information.)
+    * @param domain which hosts the cookie should be sent to while browsing
+    * @param path which path the must be included in the URL at the chosen domain for the cookie be sent to the host - '/' by default
+    * @param expires when the cookie should expire / be deleted from browser - 2 days time by default if not specified
+    * @param secure whether to use https or not (not being http instead)
+    * @param httpOnly whether the cookie should be inaccessible to javascript
+    * @param sameSite whether the cookies can be sent with cross site requests
+    */
+   constructor(name:string, value:string|null, domain:string, path?:string, expires?:string|Date|null, secure?:boolean, httpOnly?:boolean, sameSite?:'Strict'|'Lax'|'None'|null) {
+       this.name = name
+       this.value = value
+       this.domain = domain 
+       this.path = path ? path : '/'
+       //set expiry date
+       const d = new Date();
+       const year = d.getFullYear()
+       const month = d.getMonth()
+       const day = d.getDate()
+       const twoDaysTime = day + 3
+       this.expires = expires != null ? expires : new Date(year,month,twoDaysTime)
+       // this.size = size ? size : 
+       this.secure = secure != null ? secure : false
+       this.httpOnly = httpOnly ? httpOnly : false
+       this.sameSite = sameSite != null ? sameSite : 'Lax'
+   }
 
 
-    getCookieStr() {
-        var cookie:string = `${this.name}=${this.value};`
 
-        cookie += `Domain=${this.domain};`
-        cookie += `Path=${this.path};`
-        cookie += `Expires=${(this.expires as Date).toUTCString()};`
-        if (this.secure == true)
-        {
-            cookie +='Secure;'
-        }
-        if (this.httpOnly == true)
-        {
-            cookie += 'HttpOnly;'
-        }
-        cookie += `SameSite=${this.sameSite};`
-        return cookie;
-    }
 
-    print() {
-        console.log('\n'+`printing cookie:${this.getCookieStr()}`)
-    }
+   getCookieStr() {
+       var cookie:string = `${this.name}=${this.value};`
 
-    toString() {
-        return this.getCookieStr()
-    }
+       cookie += `Domain=${this.domain};`
+       cookie += `Path=${this.path};`
+       cookie += `Expires=${(this.expires as Date).toUTCString()};`
+       if (this.secure == true)
+       {
+           cookie +='Secure;'
+       }
+       if (this.httpOnly == true)
+       {
+           cookie += 'HttpOnly;'
+       }
+       cookie += `SameSite=${this.sameSite};`
+       return cookie;
+   }
+
+   print() {
+       console.log('\n'+`printing cookie:${this.getCookieStr()}`)
+   }
+
+   toString() {
+       return this.getCookieStr()
+   }
 }
 
 /**
- * Create Session Cookies with the following default properties:
- * expires in two days (as the cookie class does by default)
- * secure - true //use https
- * httpOnly - true // can be accessed via javascript
- * sameSite - Lax //whether the cookies can be sent with cross site requests
- * 
- * @param name 
- * @param value 
- * @param domain 
- */
- export function createSessionCookie(name:string, value:string|null, domain:string):Cookie
- {
-     var cname = name
-     var cvalue = value
-     
-     var cdomain = domain//which hosts can recieve a cookies
-     var path = '/'
-     var expires:string|Date|null = null
-     var secure:boolean = true//i.e. use https
-     var httpOnly = true //don't give javascript access to cookie
-     var sameSite:'Strict'|'Lax'|'None' = 'Lax'//whether the cookies can be sent with cross site requests
-     var cookie = new Cookie(cname,cvalue,cdomain,path,expires,secure,httpOnly,sameSite)
- 
-     return cookie
- }
+* Create Session Cookies with the following default properties:
+* expires in two days (as the cookie class does by default)
+* secure - true //use https
+* httpOnly - true // can be accessed via javascript
+* sameSite - Lax //whether the cookies can be sent with cross site requests
+* 
+* @param name 
+* @param value 
+* @param domain 
+*/
+export function createSessionCookie(name:string, value:string|null, domain:string):Cookie
+{
+    var cname = name
+    var cvalue = value
+    
+    var cdomain = domain//which hosts can recieve a cookies
+    var path = '/'
+    var expires:string|Date|null = null
+    var secure:boolean = true//i.e. use https
+    var httpOnly = true //don't give javascript access to cookie
+    var sameSite:'Strict'|'Lax'|'None' = 'Lax'//whether the cookies can be sent with cross site requests
+    var cookie = new Cookie(cname,cvalue,cdomain,path,expires,secure,httpOnly,sameSite)
+
+    return cookie
+}
+
+/**
+* 
+* @param cookies the string containing all the cookies you are looking
+* @param cookieName the name of the cookie that you are searching for in all the cookies
+* @param asArr whether to the results as an array of attributes
+* 
+* Note:
+* If searching cookies from node req object.
+* Each cookie is separated by a comma `,`
+* and each attribute is separated by a semicolon `;`
+*/
+export function findCookie(cookies:string, cookieName:string, asArr:boolean=false, node:boolean=true):string[]|string|undefined
+{
+  var cookieStr:string[]|string|undefined = ''
+  node ? cookieStr = findCookieNode(cookies,cookieName,asArr) : cookieStr = findCookieJS(cookies,cookieName,asArr);
+  return cookieStr
+}
+
+
+/**
+* 
+* @param cookies the string containing all the cookies you are looking
+* @param cookieName the name of the cookie that you are searching for in all the cookies
+* @param asArr whether to the results as an array of attributes
+* 
+* Note:
+* If searching cookies from node req object.
+* Each cookie is separated by a comma `;`
+* and there is only the value attribute of each cookie
+*/
+export function findCookieNode(cookies:string, cookieName:string, asArr:boolean):string|string[]|undefined
+{
+   console.log('function findCookieNode called')
+   var arrCookies = cookies.trim().split(';')
+   var cookieStrReturn:string = ''
+   var cookieAsArr:any[] = []
+   for(var i=0; i< arrCookies.length; i++) {
+      var cookieStr = arrCookies[i]
+      if (cookieStr.includes(cookieName))
+      {
+         if (asArr) 
+         {
+            console.log('returning cookie as array of attributes')
+            cookieAsArr = cookieStr.trim().split('=')
+            return cookieAsArr
+         }
+         else
+         {
+            console.log('returning cookieStr...')
+            return cookieStr
+         }
+      }
+   }
+}
+
+/**
+* 
+* @param cookies the string containing all the cookies you are looking
+* @param cookieName the name of the cookie that you are searching for in all the cookies
+* @param asArr whether to the results as an array of attributes
+* 
+* Note:
+* If searching cookies from node req object.
+* Each cookie is separated by a comma `,`
+* and each attribute is separated by a semicolon `;`
+*/
+export function findCookieJS(cookies:string, cookieName:string, asArr:boolean)
+{
+  var arr = cookies.trim().split(',')
+  arr.forEach((cookieStr) => {
+     if (cookieStr.includes(cookieName))
+     {
+        if (asArr) 
+        {
+           var cookieAsArr = cookieStr.trim().split('=')
+           return cookieAsArr
+        }
+        else
+        {
+           return cookieStr
+        }
+     }
+  })
+  return ''
+}
+
+/**
+* Method to find an attribute in a single cookie string
+* @param cookieStr single cookie in a strings
+* @param attribute single attribute you want the value of from the cookie
+* @return [attributeValue,cookie] - cookie returned as object with key value pairs
+*/
+export function findCookieAttribute(cookieStr:string, attribute:string)
+{
+  var cookie:any = {}
+  var attributeValue:string = ''
+  //split cookie into attributes
+  var attributeArr = cookieStr.trim().split(';')
+  //split attributes into key value pairs
+  attributeArr.forEach((attr) => {
+     const [key,value] = attr.trim().split('=')
+     cookie[key] = value
+     if (key == attribute)
+     {
+        attributeValue = value
+     }
+  })
+  return [attributeValue, cookie]
+}
