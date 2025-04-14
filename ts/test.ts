ce1f0cc0a496a73bfdf458bde3d682de9f41ff72
ZIshmael <38586415+Zaederx@users.noreply.github.com>
2025-04-14 13:11:07 +0200

    Create test.ts

---
+++ ts/test.ts
@@ -0,0 +1,10 @@
+import { findCookie, findCookieAttribute, createSessionCookie } from "./cookie.js"; 
+
+var cookiesStr = 'memoir-session=d19ef599-9439-444d-9141-209a1b0fd748; _csrf=LX2sCpfX5zvK2TFKAFafxnHt'
+
+var sessionCookieStr = findCookie(cookiesStr, 'memoir-session') as string
+console.log('sessionCookieStr:',sessionCookieStr)
+
+var [attribute, cookie] = findCookieAttribute(sessionCookieStr, 'memoir-session')
+console.log(attribute, '\n', cookie)
+
