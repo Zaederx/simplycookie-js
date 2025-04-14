bdedc9201106ff4a3b2dbca15d2832ed4193d10a
ZIshmael <38586415+Zaederx@users.noreply.github.com>
2025-04-14 13:40:02 +0200

    add spacing to test console logs

--- js/test.js
+++ js/test.js
@@ -1,6 +1,6 @@
 import { findCookie, findCookieAttribute } from "./cookie.js";
 var cookiesStr = 'memoir-session=d19ef599-9439-444d-9141-209a1b0fd748; _csrf=LX2sCpfX5zvK2TFKAFafxnHt';
 var sessionCookieStr = findCookie(cookiesStr, 'memoir-session');
-console.log('sessionCookieStr:', sessionCookieStr);
+console.log('sessionCookieStr:', sessionCookieStr, '\n');
 var [attribute, cookie] = findCookieAttribute(sessionCookieStr, 'memoir-session');
-console.log(attribute, '\n', cookie);
+console.log('attribute:', attribute, '\n', 'cookie:', cookie);
--- ts/test.ts
+++ ts/test.ts
@@ -3,8 +3,8 @@ import { findCookie, findCookieAttribute, createSessionCookie } from "./cookie.j
 var cookiesStr = 'memoir-session=d19ef599-9439-444d-9141-209a1b0fd748; _csrf=LX2sCpfX5zvK2TFKAFafxnHt'
 
 var sessionCookieStr = findCookie(cookiesStr, 'memoir-session') as string
-console.log('sessionCookieStr:',sessionCookieStr)
+console.log('sessionCookieStr:',sessionCookieStr, '\n')
 
 var [attribute, cookie] = findCookieAttribute(sessionCookieStr, 'memoir-session')
-console.log(attribute, '\n', cookie)
+console.log('attribute:',attribute, '\n', 'cookie:',cookie)
 
