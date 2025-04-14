5d392b69c5b5cfa6e12f5b6b21b31e4210945ae0
ZIshmael <38586415+Zaederx@users.noreply.github.com>
2025-04-14 13:10:12 +0200

    Create test.js

---
+++ js/test.js
@@ -0,0 +1,6 @@
+import { findCookie, findCookieAttribute } from "./cookie.js";
+var cookiesStr = 'memoir-session=d19ef599-9439-444d-9141-209a1b0fd748; _csrf=LX2sCpfX5zvK2TFKAFafxnHt';
+var sessionCookieStr = findCookie(cookiesStr, 'memoir-session');
+console.log('sessionCookieStr:', sessionCookieStr);
+var [attribute, cookie] = findCookieAttribute(sessionCookieStr, 'memoir-session');
+console.log(attribute, '\n', cookie);
